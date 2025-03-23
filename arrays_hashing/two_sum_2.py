e36bbbc15030e8ed5121483586f829dccdcda74d
Ahmed Khan <khanahmed925@gmail.com>
2025-03-23 14:31:08 +0000

    two sum 2

---
+++ arrays_hashing/two_sum_2.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+def twoSum(numbers: list[int], target: int) -> list[int]:
+    L = 0
+    R = len(numbers) - 1
+
+    while L < R:
+        curr_sum = numbers[L] + numbers[R]
+        if curr_sum == target:
+            # Adding 1 to convert from 0-indexed to 1-indexed
+            return [L + 1, R + 1]
+        elif curr_sum < target:
+            # If sum is too small, move left pointer to increase the sum
+            L += 1
+        else:
+            # If sum is too large, move right pointer to decrease the sum
+            R -= 1
+
+    # The problem guarantees a solution, so this should never be reached
+    return []
+
+# Test cases
+test_cases = [
+    ([2,7,11,15], 9),
+    ([2,3,4], 6),
+    ([-1,0], -1)
+]
+
+for numbers, target in test_cases:
+    result = twoSum(numbers, target)
+    print(f"Input: numbers = {numbers}, target = {target}")
+    print(f"Output: {result}\n")
