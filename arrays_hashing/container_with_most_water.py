274aab38b862df21975d24f87899c5116a67a673
Ahmed Khan <khanahmed925@gmail.com>
2025-03-24 12:56:29 +0000

    container with most water

---
+++ arrays_hashing/container_with_most_water.py
@@ -0,0 +1,28 @@
+def maxArea(height):
+    # Initialize two pointers: L at start, R at end
+    L = 0
+    R = len(height) - 1
+    max_water = 0
+
+    # Continue until pointers meet
+    while L < R:
+        # Calculate current water area
+        # Width is distance between pointers (R - L)
+        # Height is minimum of the two lines
+        curr_water = min(height[L], height[R]) * (R - L)
+
+        # Update max_water if current is larger
+        max_water = max(max_water, curr_water)
+
+        # Move the pointer pointing to the shorter line
+        # Because moving the taller line inward won't increase area
+        if height[L] < height[R]:
+            L += 1
+        else:
+            R -= 1
+
+    return max_water
+
+# Test cases
+print(maxArea([1,8,6,2,5,4,8,3,7]))  # Output: 49
+print(maxArea([1,1]))  # Output: 1
