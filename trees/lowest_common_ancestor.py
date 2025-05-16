0373e3e0bc1aa29f6eb15abd3d7414056d1b7b1b
Ahmed Khan <khanahmed925@gmail.com>
2025-05-16 20:10:12 +0000

    lca lc link added

--- trees/lowest_common_ancestor.py
+++ trees/lowest_common_ancestor.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x, left=None, right=None):
