65d79693a16c724fa6c1a9fea9567ab3af53090f
Ahmed Khan <khanahmed925@gmail.com>
2025-06-04 17:36:09 +0000

    single number

---
+++ bit_manipulation/single_number.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/single-numbe
+
+# TC - O(N), SC - O(1)
+def singleNumber(nums):
+    result = 0
+    for num in nums:
+        result ^= num
+    return result
+
+# Test cases
+print(singleNumber([2, 2, 1]))  # Output: 1
+print(singleNumber([4, 1, 2, 1, 2]))  # Output: 4
+print(singleNumber([1]))  # Output: 1
