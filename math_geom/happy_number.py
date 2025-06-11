889de205a6d9bbfeea5df439b6ec02ca9f1d0b9c
Ahmed Khan <khanahmed925@gmail.com>
2025-06-11 16:01:37 +0000

    happy number

---
+++ math_geom/happy_number.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/happy-number/
+
+# TC - O(LogN), SC - O(LogN)
+def isHappy(n: int) -> bool:
+    seen = set()
+    while n != 1:
+        if n in seen:
+            return False
+        seen.add(n)
+        n = sum(int(digit) ** 2 for digit in str(n))
+        print(n)
+    return True
+
+
+test_cases = [
+    # (19, True),
+    (2, False),
+    # (7, True),
+    # (23, True),
+    # (4, False)
+]
+
+for num, expected in test_cases:
+    result = isHappy(num)
+    print(f"Input: {num}, Output: {result}, Expected: {expected}, {'Pass' if result == expected else 'Fail'}")
