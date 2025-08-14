0d4115dca42d715a985b3c4e6b2e81a1b0652485
Ahmed Khan <khanahmed925@gmail.com>
2025-08-14 18:43:28 +0000

    search sort rot

--- binary_search/search_sorted_rotated.py
+++ binary_search/search_sorted_rotated.py
@@ -1,26 +1,26 @@
 # https://leetcode.com/problems/search-in-rotated-sorted-array/
 
 # TC - O(N), SC - O(1)
-def search(nums, target):
-    L, R = 0, len(nums) - 1
+def search(A, target):
+    L, R = 0, len(A) - 1
 
     while L <= R:
         mid = (L + R) // 2
 
-        if nums[mid] == target:
+        if A[mid] == target:
             return mid
 
         # Check if left half is sorted
-        if nums[L] <= nums[mid]:
+        if A[L] <= A[mid]:
             # Check if target is in left half
-            if nums[L] <= target < nums[mid]:
+            if A[L] <= target < A[mid]:
                 R = mid - 1
             else:
                 L = mid + 1
         # right half is sorted
         else:
             # Check if target is in right half
-            if nums[mid] < target <= nums[R]:
+            if A[mid] < target <= A[R]:
                 L = mid + 1
             else:
                 R = mid - 1
@@ -32,9 +32,9 @@ print(search([4, 5, 6, 7, 0, 1, 2], 0))  # Expected: 4
 print(search([4, 5, 6, 7, 0, 1, 2], 3))  # Expected: -1
 print(search([1], 0))  # Expected: -1
 
-# In nums[left] <= target < nums[mid]:
-# <= includes nums[left] as a possible target value.
-# < excludes nums[mid] since it’s checked separately.
-# In nums[mid] < target <= nums[right]:
-# < excludes nums[mid] since it’s checked separately.
-# <= includes nums[right] as a possible target value.
+# In A[left] <= target < A[mid]:
+# <= includes A[left] as a possible target value.
+# < excludes A[mid] since it’s checked separately.
+# In A[mid] < target <= A[right]:
+# < excludes A[mid] since it’s checked separately.
+# <= includes A[right] as a possible target value.
