1548a9d340b1ae049b298407eb42cac2ee37b558
Ahmed Khan <khanahmed925@gmail.com>
2025-09-02 10:40:34 +0000

    product except self

--- arrays_hashing/product_array_except_self.py
+++ arrays_hashing/product_array_except_self.py
@@ -1,22 +1,22 @@
 # https://leetcode.com/problems/product-of-array-except-self/description/
 
 # TC - O(N), SC - O(1)
-def productExceptSelf(nums):
-    n = len(nums)
+def productExceptSelf(A):
+    N = len(A)
     # Initialize output array
-    answer = [1] * n
+    answer = [1] * N
 
     # First pass: compute products of all elements to the left of each element
     left_product = 1
-    for i in range(n):
+    for i in range(N):
         answer[i] = left_product
-        left_product *= nums[i]
+        left_product *= A[i]
 
     # Second pass: multiply by products of all elements to the right of each element
     right_product = 1
-    for i in range(n-1, -1, -1):
+    for i in range(N-1, -1, -1):
         answer[i] *= right_product
-        right_product *= nums[i]
+        right_product *= A[i]
 
     return answer
 
