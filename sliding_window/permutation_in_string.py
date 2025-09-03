e55b5a485d977367c7de8c97f06a5b3124154f40
Ahmed Khan <khanahmed925@gmail.com>
2025-09-03 14:41:46 +0000

    permutation in string

--- sliding_window/permutation_in_string.py
+++ sliding_window/permutation_in_string.py
@@ -8,25 +8,25 @@ def checkInclusion(s1: str, s2: str) -> bool:
 
     # Initialize frequency arrays for s1 and the sliding window
     s1_count = [0] * 26
-    window_count = [0] * 26
+    s2_count = [0] * 26
 
     # Count frequencies of characters in s1 and first window of s2
     for i in range(len(s1)):
         s1_count[ord(s1[i]) - ord('a')] += 1
-        window_count[ord(s2[i]) - ord('a')] += 1
+        s2_count[ord(s2[i]) - ord('a')] += 1
 
     # Check if the first window matches
-    if s1_count == window_count:
+    if s1_count == s2_count:
         return True
 
     # Slide the window over s2
     for i in range(len(s1), len(s2)):
         # Add new character to the window
-        window_count[ord(s2[i]) - ord('a')] += 1
+        s2_count[ord(s2[i]) - ord('a')] += 1
         # Remove character from the start of the previous window
-        window_count[ord(s2[i - len(s1)]) - ord('a')] -= 1
+        s2_count[ord(s2[i - len(s1)]) - ord('a')] -= 1
         # Check if current window matches s1's frequency
-        if s1_count == window_count:
+        if s1_count == s2_count:
             return True
 
     return False
